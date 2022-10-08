352cf6ef09db01a26370b8355d4ed907fd580adb
Arthaud Proust <arthaud.proust@mink-agency.com>
2022-10-08 18:58:19 +0200

    ci: remove webhook auth

--- app/Http/Middleware/Webhook.php
+++ app/Http/Middleware/Webhook.php
@@ -10,14 +10,14 @@ class Webhook
 {
     public function handle(Request $request, Closure $next)
     {
-        $githubPayload = $request->getContent();
-        $githubHash = $request->header('X-Hub-Signature');
-        $localToken = config('app.deploy_secret');
-        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
-
-        if (hash_equals($githubHash, $localHash)) {
-            abort(403);
-        }
+//        $githubPayload = $request->getContent();
+//        $githubHash = $request->header('X-Hub-Signature');
+//        $localToken = config('app.deploy_secret');
+//        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+//
+//        if (hash_equals($githubHash, $localHash)) {
+//            abort(403);
+//        }
 
         return $next($request);
     }
