27ef307ef7e0adb28f3980efe8adef5753471870
Arthaud Proust <arthaud.proust@mink-agency.com>
2022-10-23 16:48:04 +0200

    feat: clothing image loading

--- resources/js/Components/Form/ClothingImagesInput.vue
+++ resources/js/Components/Form/ClothingImagesInput.vue
@@ -1,6 +1,7 @@
 <script>
 import {XMarkIcon} from '@heroicons/vue/24/solid';
 import Draggable from 'vuedraggable';
+import {v4 as uuid} from 'uuid';
 
 export default {
     components: {
@@ -8,14 +9,18 @@ export default {
         XMarkIcon
     },
     props: {
-        uploadedImages: {
+        alreadyUploadedImages: {
             type: Array,
             default: [],
         },
     },
     mounted() {
         this.images = [
-            ...this.uploadedImages
+            ...this.alreadyUploadedImages.map(img => ({
+                id: uuid(),
+                uploading: false,
+                src: img
+            }))
         ];
     },
     data() {
@@ -34,6 +39,14 @@ export default {
             e.target.files.forEach(image => this.uploadImage(image));
         },
         async uploadImage(image) {
+            const imgId = uuid();
+
+            this.images.push({
+                id: imgId,
+                uploading: true,
+                src: null,
+            })
+
             const formData = new FormData();
             formData.set('image', image);
 
@@ -47,14 +60,16 @@ export default {
                 }
             );
 
-            this.images = [
-                ...this.images,
-                response.data
-            ];
+            this.images = this.images.filter(img => img.id !== imgId);
+            this.images.push({
+                id: imgId,
+                uploading: false,
+                src: response.data
+            })
         },
-        async deleteImage(imagePath) {
+        async deleteImage(imageSrc) {
             const data = {
-                image_path: imagePath
+                image_path: imageSrc
             };
 
             await axios.post(
@@ -66,12 +81,25 @@ export default {
                     }
                 });
 
-            this.images = this.images.filter(img => img !== imagePath);
+            this.images = this.images.filter(img => img.src !== imageSrc);
+        },
+    },
+    computed: {
+        allUploadedImagesSrc() {
+            return this.images.reduce((a, b) => {
+                if (!b.uploading) {
+                    a.push(b.src)
+                }
+                return a;
+            }, []);
         }
     },
     watch: {
-        images() {
-            this.$emit('change:images', this.images)
+        images: {
+            handler() {
+                this.$emit('change:images', this.allUploadedImagesSrc)
+            },
+            deep: true
         }
     }
 }
@@ -88,13 +116,26 @@ export default {
                 <div
                     class="relative overflow-hidden rounded-md w-1/3"
                 >
+                    <div v-if="element.uploading"
+                         class="flex flex-row justify-center items-center w-full aspect-3/4 object-cover bg-neutral-100 text-neutral-400 font-semibold">
+                        <svg class="animate-spin -ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg"
+                             fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                    stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor"
+                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        Envoi...
+                    </div>
                     <img
+                        v-else
                         class="w-full aspect-3/4 object-cover"
-                        :src="'/storage/' + element"
+                        :src="'/storage/' + element.src"
                     />
                     <div class="absolute top-0 right-0 flex flex-col gap-2 p-2">
-                        <button v-if="!element.startsWith('test')" type="button" class="btn-icon-danger"
-                                @click="deleteImage(element)">
+                        <button v-if="!element.uploading  && !element.src.startsWith('test')" type="button"
+                                class="btn-icon-danger"
+                                @click="deleteImage(element.src)">
                             <XMarkIcon class="h-4 w-4"/>
                         </button>
                     </div>
